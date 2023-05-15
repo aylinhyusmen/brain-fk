fa7690961af68bfd9bc9efa6bd8b11e53487e57a
Aylin Hyusmen <ahyusmen@icloud.com>
2023-05-15 15:58:55 +0300

    removed a word

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'certified \n brainfuck \n developer',
+            'brainfuck \n developer',
             {
                 font: font,
                 size: 0.5,
